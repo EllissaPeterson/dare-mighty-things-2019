960d7193ed2f1214048269e12640c1a2eb33e868
flarphengarg <anders.erik.lie@gmail.com>
2019-10-26 15:07:58 -0500

    Put some stuff in readme, commented random stuff out in sketch

--- src/sketch.js
+++ src/sketch.js
@@ -1,15 +1,18 @@
 export default function sketch (p) {
-  let rotation = 0;
+  //let rotation = 0;
+  let audioIn;
 
   p.setup = function () {
     p.createCanvas(600, 400, p.WEBGL);
   };
 
+  /*
   p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
     if (props.rotation){
       rotation = props.rotation * Math.PI / 180;
     }
   };
+  */
 
   p.draw = function () {
     p.background(100);
