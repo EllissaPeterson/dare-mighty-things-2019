792ad037dd8a46cce411dc61fb81d5f8552598b6
EllissaPeterson <ellissa@iastate.edu>
2019-10-26 13:13:46 -0500

    firebase stuff

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+var config = {
+  apiKey: "AIzaSyDGcka-5SOrH54E4zUStNDkF345CPhOOdY",
+  authDomain: "dare-mighty-things-2019.firebaseapp.com",
+  databaseURL: "https://dare-mighty-things-2019.firebaseio.com",
+  storageBucket: "dare-mighty-things-2019.appspot.com",
+};
+
+const Home = () => (
+  <header className="App-header">
+    <img src="logo.jpg" className="App-logo" alt="logo" />
+  </header>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          Hello World
-          <br/>
-          My First React App "HELLO WORLD"
-        </p>
-        Want to Learn React Click on this link Thanks!
-        <a
-          className="App-link"
-          href="https://drive.google.com/file/d/1J74__LDksIHpfQnEZFitW4LRNMLXlp2Y/view?fbclid=IwAR0PyhFWSWhaY8iHsFpYfKOFbiBP1raJHBIk9rVNfIuC_WVALBc0d9ntS7c"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         The Road to Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   );
 }
 
