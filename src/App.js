afad51d4c64490e170e0b0ed6b6990814566720e
EllissaPeterson <ellissa@iastate.edu>
2019-10-26 14:43:51 -0500

    all the good stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import {Container, Row, Col} from 'react-bootstrap';
+import axios from 'axios';
 
 var config = {
   apiKey: "AIzaSyDGcka-5SOrH54E4zUStNDkF345CPhOOdY",
@@ -15,14 +17,28 @@ const Home = () => (
   </header>
 )
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-          <Route exact path="/" component={Home} />
-      </div>
-    </Router>
-  );
+export default class App extends React.Component {
+  state = {
+    persons: [],
+    name: ""
+  }
+  componentDidMount() {
+    //coms-309-bs-4.misc.iastate.edu:8080/test/hello
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        console.log(res.data[0]);
+        this.setState({persons : res.data});
+        console.log(this.state.persons[0].name);
+        this.setState({name : res.data[0].name});
+      })
+  }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+            <Route exact path="/" component={Home} />
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
