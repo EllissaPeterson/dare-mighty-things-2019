36eab02e42c8d3f71b4e6a7cf5772186cbb44a89
4N24 <46408746+4N24@users.noreply.github.com>
2019-10-26 21:41:38 -0500

    Link on icon to main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, withRouter} from 'react-router-dom';
 import { Button } from 'reactstrap';
 import MainPage from './components/MainPage';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketch';
 import CHeader from './components/CustomHeader';
+
 
 const Home = () => (
   <header className="App-header">
@@ -19,8 +20,8 @@ function App() {
     <Router>
       <CHeader/>
       <div className="App">
-          <Route exact path="/" component={Home} />
-          <Route exact path="/mainPage" component={MainPage} />
+          <Route exact path="/" component={withRouter(Home)} />
+          <Route exact ="/mainPage" component={MainPage} />
       </div>
     </Router>
   );
