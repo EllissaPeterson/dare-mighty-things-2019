652b7b074b64438f67751f184a9b28c3ecfda946
EllissaPeterson <ellissa@iastate.edu>
2019-10-26 22:27:53 -0500

    pulsing pineapple

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { Button } from 'reactstrap';
 import MainPage from './components/MainPage';
+import PulsingPineapple from './components/PulsingPineapple';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketch';
 import CHeader from './components/CustomHeader';
@@ -10,7 +11,8 @@ import CHeader from './components/CustomHeader';
 const Home = () => (
   <header className="App-header">
     <br></br>
-    <Button color="info" size="lg" href="/mainPage" block>Main Page</Button>
+    <Button color="info" size="lg" href="/mainPage" block>Normal Mode</Button>
+    <Button color="info" size="lg" href="/pulsingPineapple" block>Pulsing Pineapple</Button>
     <P5Wrapper sketch={sketch} />
   </header>
 )
@@ -22,6 +24,7 @@ function App() {
       <div className="App">
           <Route exact path="/" component={Home} />
           <Route exact path="/mainPage" component={MainPage} />
+          <Route exact path="/pulsingPineapple" component={PulsingPineapple} />
       </div>
     </Router>
   );
