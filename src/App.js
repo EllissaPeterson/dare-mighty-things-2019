34757c37e3ed0452490139fba46e086c310c63dd
4N24 <46408746+4N24@users.noreply.github.com>
2019-10-26 20:31:10 -0500

    quick update on header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { Button } from 'reactstrap';
 import MainPage from './components/MainPage';
-
+import CHeader from './components/CustomHeader';
 const Home = () => (
   <header className="App-header">
     <Button color="info" size="lg" href="/mainPage" block>Main Page</Button>
@@ -13,6 +13,7 @@ const Home = () => (
 function App() {
   return (
     <Router>
+      <CHeader/>
       <div className="App">
           <Route exact path="/" component={Home} />
           <Route exact path="/mainPage" component={MainPage} />
