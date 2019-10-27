61f041b6272a791180072a8ab87a0bb7e3f792b2
EllissaPeterson <ellissa@iastate.edu>
2019-10-26 23:12:58 -0500

    folders

--- src/components/pulsingPineapple/PineappleVisualiser.js
+++ src/components/pulsingPineapple/PineappleVisualiser.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import P5Wrapper from 'react-p5-wrapper';
-import sketch from '../sketch';
+import sketch from '../../sketch';
 
 class PineappleVisualiser extends Component {
     constructor(props) {
